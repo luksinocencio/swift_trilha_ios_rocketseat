0c8679c312e8aca85005d31c45d9b40df27759db
luksinocencio <luksinocencio@gmail.com>
2025-01-29 02:37:58 -0300

    *

---
+++ 01 - Primeiros passos/Reminder/Reminder/Sources/Features/LoginBottomSheet/LoginBottomSheetViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class LoginBottomSheetViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- 01 - Primeiros passos/Reminder/Reminder/Sources/Features/Splash/SplashViewController.swift
+++ 01 - Primeiros passos/Reminder/Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -5,22 +5,31 @@ class SplashViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setup()
     }
     
+    override func viewWillLayoutSubviews() {
+            super.viewWillLayoutSubviews()
+            view.frame = UIScreen.main.bounds // <- Garante que a tela ocupe toda a altura
+        }
+    
     private func setup() {
         self.view.addSubview(contentView)
+        self.navigationController?.navigationBar.isHidden = true
+        self.view.backgroundColor = Colors.primaryRedBase
+        
         setupConstraints()
     }
     
     private func setupConstraints() {
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        
         NSLayoutConstraint.activate([
             contentView.topAnchor.constraint(equalTo: view.topAnchor),
             contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
     }
 }
