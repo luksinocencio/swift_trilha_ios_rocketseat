73179a7b60390ee1dd61e0c297c1cbf7e602ba2d
luksinocencio <luksinocencio@gmail.com>
2025-02-26 03:52:44 -0300

    *

--- 01 - Primeiros passos/Reminder/Reminder/Sources/Features/Scenes/NewReceipt/NewReceiptViewController.swift
+++ 01 - Primeiros passos/Reminder/Reminder/Sources/Features/Scenes/NewReceipt/NewReceiptViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class NewReceiptViewController: UIViewController {
     private let newReceiptView  = NewReceiptView()
+    private let viewModel = NewReceiptViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +23,21 @@ class NewReceiptViewController: UIViewController {
     
     private func setupActions() {
         newReceiptView.backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        newReceiptView.addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
     }
     
     @objc
     private func backButtonTapped() {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc
+    private func addButtonTapped() {
+        let remedy = newReceiptView.remedyInput.getText()
+        let time = newReceiptView.timeInput.getText()
+        let recurrence = newReceiptView.recurrenceInput.getText()
+        let takeNow = false
+        
+        viewModel.addReceipt(remedy: remedy, time: time, recurrence: recurrence, takeNow: takeNow)
     }
 }
---
+++ 01 - Primeiros passos/Reminder/Reminder/Sources/Features/Scenes/NewReceipt/ViewModel/NewReceiptViewModel.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class NewReceiptViewModel {
+    func addReceipt(remedy: String, time: String, recurrence: String, takeNow: Bool) {
+        DBHelper.shared.insertReceipt(remedy: remedy, time: time, recurrence: recurrence, takeNow: takeNow)
+    }
+}
