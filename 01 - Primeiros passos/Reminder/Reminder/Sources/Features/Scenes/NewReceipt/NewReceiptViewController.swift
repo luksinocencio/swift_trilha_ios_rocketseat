0bcedbd30851a33237f3bed5cc716ee94fd4f224
luksinocencio <luksinocencio@gmail.com>
2025-03-31 18:25:59 -0300

    *

--- 01 - Primeiros passos/Reminder/Reminder/Sources/Features/Scenes/NewReceipt/NewReceiptViewController.swift
+++ 01 - Primeiros passos/Reminder/Reminder/Sources/Features/Scenes/NewReceipt/NewReceiptViewController.swift
@@ -1,8 +1,19 @@
+import Lottie
 import UIKit
 
 class NewReceiptViewController: UIViewController {
     private let newReceiptView  = NewReceiptView()
     private let viewModel = NewReceiptViewModel()
+    
+    private let successAnimationView: LottieAnimationView = {
+        let animationView = LottieAnimationView(name: "success")
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .playOnce
+        animationView.animationSpeed = 1.0
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.isHidden = true
+        return animationView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -12,6 +23,7 @@ class NewReceiptViewController: UIViewController {
     private func setupView() {
         view.backgroundColor = Colors.gray800
         view.addSubview(newReceiptView)
+        view.addSubview(successAnimationView)
         setupConstraints()
         setupActions()
     }
@@ -19,11 +31,27 @@ class NewReceiptViewController: UIViewController {
     private func setupConstraints() {
         newReceiptView.translatesAutoresizingMaskIntoConstraints = false
         setupContentViewToBounds(contentView: newReceiptView)
+        
+        NSLayoutConstraint.activate([
+            successAnimationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            successAnimationView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            successAnimationView.heightAnchor.constraint(equalToConstant: 120),
+            successAnimationView.widthAnchor.constraint(equalToConstant: 120)
+        ])
     }
     
     private func setupActions() {
         newReceiptView.backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
         newReceiptView.addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+    }
+    
+    private func playSuccessAnimation() {
+        successAnimationView.isHidden = false
+        successAnimationView.play { [weak self] finished in
+            if finished {
+                self?.successAnimationView.isHidden = true
+            }
+        }
     }
     
     @objc
@@ -37,7 +65,7 @@ class NewReceiptViewController: UIViewController {
         let time = newReceiptView.timeInput.getText()
         let recurrence = newReceiptView.recurrenceInput.getText()
         let takeNow = false
-        
+        playSuccessAnimation()
         viewModel.addReceipt(remedy: remedy, time: time, recurrence: recurrence, takeNow: takeNow)
     }
 }
