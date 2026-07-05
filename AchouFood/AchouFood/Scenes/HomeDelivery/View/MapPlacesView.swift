8f24e2d444f9ba1697d2bee1f12058d1e2f79251
luksinocencio <luksinocencio@gmail.com>
2026-07-05 13:03:17 -0300

    *

--- AchouFood/AchouFood/Scenes/HomeDelivery/View/MapPlacesView.swift
+++ AchouFood/AchouFood/Scenes/HomeDelivery/View/MapPlacesView.swift
@@ -13,6 +13,9 @@ class MapPlacesView: UIView {
     private var allPlaces: [Place] = []
     private var filteredPlaces: [Place] = []
     
+    var onPinSelected: ((Place) -> Void)?
+    var onPinDeselected: (() -> Void)?
+    
     private lazy var mapView: MKMapView = {
         let view = MKMapView()
         view.showsCompass = false
@@ -104,9 +107,12 @@ extension MapPlacesView: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         view.image = UIImage(named: MapViewConstants.redPin)
+        guard let annotation = view.annotation as? PlaceAnnotation else { return }
+        onPinSelected?(annotation.place)
     }
     
     func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
         view.image = UIImage(named: MapViewConstants.blackPin)
+        onPinDeselected?()
     }
 }
